fb6f881ab9c61b7b06635f5d4c506043c5d03996
Varun Upadhyay <varun.u28@gmail.com>
2017-11-24 11:12:34 -0800

    Solved 2 stack problems

--- stacksandqueues/src/main/java/IsStringWellFormed.java
+++ stacksandqueues/src/main/java/IsStringWellFormed.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 public class IsStringWellFormed {
 
     /*
@@ -6,6 +8,24 @@ public class IsStringWellFormed {
 
     public static boolean isWellFormed(String s) {
 
-        return false;
+        String start = "{[(";
+        String end = "}])";
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+
+            if (start.indexOf(c) != -1) {
+                stack.push(c);
+            }
+            else {
+                char poppedChar = stack.pop();
+                if (start.indexOf(poppedChar) != end.indexOf(c)) {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
     }
 }
--- stacksandqueues/src/main/java/StackWithMax.java
+++ stacksandqueues/src/main/java/StackWithMax.java
@@ -1,22 +1,43 @@
+import java.util.ArrayList;
+import java.util.Stack;
+
 public class StackWithMax {
 
     /*
     9.1
     */
+    public int maxVal = Integer.MIN_VALUE;
+    Stack<ArrayList<Integer>> stack = new Stack<>();
 
     public StackWithMax() {
 
     }
 
     public Integer max() {
-        return 0;
+
+        ArrayList<Integer> arrayList = stack.peek();
+        return arrayList.get(1);
+
     }
 
     public Integer pop() {
-        return 0;
+
+        ArrayList<Integer> arrayList = stack.pop();
+        return arrayList.get(0);
+
     }
 
     public void push(Integer x) {
 
+        if (x > maxVal) {
+            maxVal = x;
+        }
+
+        ArrayList<Integer> arrayList = new ArrayList<>();
+
+        arrayList.add(x);
+        arrayList.add(maxVal);
+
+        stack.push(arrayList);
     }
 }
