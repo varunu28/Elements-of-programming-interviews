2acea36521d653a22d15107e0830022cf52af73d
varunu28 <varun.u28@gmail.com>
2019-11-02 10:06:28 -0700

    Added StackWithMax.java

--- stacksandqueues/src/main/java/StackWithMax.java
+++ stacksandqueues/src/main/java/StackWithMax.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 import java.util.Stack;
 
 public class StackWithMax {
@@ -6,22 +5,36 @@ public class StackWithMax {
     /*
     9.1
     */
-
+    Stack<Integer> stack;
+    Stack<Integer> maxStack;
     public StackWithMax() {
-
+        stack = new Stack<>();
+        maxStack = new Stack<>();
     }
 
     public Integer max() {
-
+        if (!maxStack.isEmpty()) {
+            return maxStack.peek();
+        }
         return -1;
     }
 
     public Integer pop() {
-
-        return -1;
+        if (stack.isEmpty()) {
+            return -1;
+        }
+        int popped = stack.pop();
+        maxStack.pop();
+        return popped;
     }
 
     public void push(Integer x) {
-
+        stack.push(x);
+        if (maxStack.isEmpty()) {
+            maxStack.push(x);
+        }
+        else {
+            maxStack.push(Math.max(maxStack.peek(), x));
+        }
     }
 }
